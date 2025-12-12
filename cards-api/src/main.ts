ead1f4764b1cd6315fe252a7aa478ed1a1139237
Yauheni Kuntsevich <cmaczok@gmail.com>
2025-12-12 22:20:20 +0300

    chore: add swagger

--- cards-api/src/main.ts
+++ cards-api/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Magister's Cards")
+    .setDescription("Magister's Cards API")
+    .setVersion('0.0.1')
+    .addTag('Magister')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
