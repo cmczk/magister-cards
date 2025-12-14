671463677aeed2854a7a15211a0b6b96c78aa382
Yauheni Kuntsevich <cmaczok@gmail.com>
2025-12-14 21:48:15 +0300

    feat: add get card for admin dto

--- cards-api/src/admins/admins.controller.ts
+++ cards-api/src/admins/admins.controller.ts
@@ -2,6 +2,7 @@ import {
   Body,
   Controller,
   Get,
+  NotFoundException,
   Param,
   ParseIntPipe,
   Post,
@@ -16,6 +17,8 @@ import { AuthService } from 'src/auth/auth.service';
 import { LoginAdminDto } from './dto/login-admin.dto';
 import type { Request, Response } from 'express';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { GetCardForUpdateDto } from './dto/get-card-for-update.dto';
+import { Language } from 'src/generated/prisma/enums';
 
 @Controller('admins')
 export class AdminsController {
@@ -27,10 +30,79 @@ export class AdminsController {
   @UseGuards(AuthGuard)
   @ApiOperation({ summary: 'Получить информацию о карте для редактирования' })
   @Get('cards/:id')
-  async getCardInfoForUpdate(@Param('id', ParseIntPipe) id: number) {
+  async getCardInfoForUpdate(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<GetCardForUpdateDto> {
     const card = await this.adminsService.getMagisterCardForUpdate(id);
 
-    return card;
+    if (!card) {
+      throw new NotFoundException();
+    }
+
+    return {
+      suit: card.suit,
+      rank: card.rank,
+      nameBe:
+        card.magisterCardNames.find((name) => name.language === Language.BE)
+          ?.name ?? '',
+      nameRu:
+        card.magisterCardNames.find((name) => name.language === Language.RU)
+          ?.name ?? '',
+      nameEn:
+        card.magisterCardNames.find((name) => name.language === Language.EN)
+          ?.name ?? '',
+      slugBe:
+        card.magisterCardNames.find((name) => name.language === Language.BE)
+          ?.slug ?? '',
+      slugRu:
+        card.magisterCardNames.find((name) => name.language === Language.RU)
+          ?.slug ?? '',
+      slugEn:
+        card.magisterCardNames.find((name) => name.language === Language.EN)
+          ?.slug ?? '',
+      shortDescriptionBe:
+        card.magisterCardDescriptions.find(
+          (desc) => desc.language === Language.BE,
+        )?.shortDescription ?? '',
+      shortDescriptionRu:
+        card.magisterCardDescriptions.find(
+          (desc) => desc.language === Language.RU,
+        )?.shortDescription ?? '',
+      shortDescriptionEn:
+        card.magisterCardDescriptions.find(
+          (desc) => desc.language === Language.EN,
+        )?.shortDescription ?? '',
+      longDescriptionBe:
+        card.magisterCardDescriptions.find(
+          (desc) => desc.language === Language.BE,
+        )?.longDescription ?? '',
+      longDescriptionRu:
+        card.magisterCardDescriptions.find(
+          (desc) => desc.language === Language.RU,
+        )?.longDescription ?? '',
+      longDescriptionEn:
+        card.magisterCardDescriptions.find(
+          (desc) => desc.language === Language.EN,
+        )?.longDescription ?? '',
+      divinatoryMeaningBe:
+        card.magisterCardDivinations.find((div) => div.language === Language.BE)
+          ?.divinatoryMeaning ?? '',
+      divinatoryMeaningRu:
+        card.magisterCardDivinations.find((div) => div.language === Language.RU)
+          ?.divinatoryMeaning ?? '',
+      divinatoryMeaningEn:
+        card.magisterCardDivinations.find((div) => div.language === Language.EN)
+          ?.divinatoryMeaning ?? '',
+      divinatoryInterpretionBe:
+        card.magisterCardDivinations.find((div) => div.language === Language.BE)
+          ?.divinatoryInterpretation ?? '',
+      divinatoryInterpretionRu:
+        card.magisterCardDivinations.find((div) => div.language === Language.RU)
+          ?.divinatoryInterpretation ?? '',
+      divinatoryInterpretionEn:
+        card.magisterCardDivinations.find((div) => div.language === Language.EN)
+          ?.divinatoryInterpretation ?? '',
+    };
   }
 
   @Post('create')
@@ -43,8 +115,6 @@ export class AdminsController {
     @Body() data: LoginAdminDto,
     @Res({ passthrough: true }) response: Response,
   ) {
-    console.debug(data);
-
     const authData = await this.authService.loginAdmin(
       data.email,
       data.password,
--- cards-api/src/admins/dto/get-card-for-update.dto.ts
+++ cards-api/src/admins/dto/get-card-for-update.dto.ts
@@ -3,6 +3,61 @@ import { ApiProperty } from '@nestjs/swagger';
 export class GetCardForUpdateDto {
   @ApiProperty()
   suit: string;
+
   @ApiProperty()
   rank: string;
+
+  @ApiProperty()
+  slugBe: string;
+
+  @ApiProperty()
+  nameBe: string;
+
+  @ApiProperty()
+  slugRu: string;
+
+  @ApiProperty()
+  nameRu: string;
+
+  @ApiProperty()
+  slugEn: string;
+
+  @ApiProperty()
+  nameEn: string;
+
+  @ApiProperty()
+  shortDescriptionBe: string;
+
+  @ApiProperty()
+  longDescriptionBe: string;
+
+  @ApiProperty()
+  shortDescriptionRu: string;
+
+  @ApiProperty()
+  longDescriptionRu: string;
+
+  @ApiProperty()
+  shortDescriptionEn: string;
+
+  @ApiProperty()
+  longDescriptionEn: string;
+
+  @ApiProperty()
+  divinatoryMeaningBe: string;
+
+  @ApiProperty()
+  divinatoryInterpretionBe: string;
+
+  @ApiProperty()
+  divinatoryMeaningRu: string;
+
+  @ApiProperty()
+  divinatoryInterpretionRu: string;
+
+  @ApiProperty()
+  divinatoryMeaningEn: string;
+
+  @ApiProperty()
+  divinatoryInterpretionEn: string;
 }
